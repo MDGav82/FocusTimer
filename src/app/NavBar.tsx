14fb0556fcc8fa580dc8bb610bb002e6355700c2
claraFreund <c.freund@notrelec.fr>
2026-05-21 10:04:08 +0200

    feat(NavBar): working navbar with active links

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 export default function NavBar() {
     return (
-        <nav>
-            <Link to="/">Focustimer</Link>
-            <Link to="/dashboard">Dashboard</Link>
-            <Link to="/settings">Paramètres</Link>
+        <nav className="flex gap-2">
+            <NavLink to="/" className={({isActive}) => isActive ? "text-blue-500" : ""}>Focustimer</NavLink>
+            <NavLink to="/dashboard" className={({isActive}) => isActive ? "text-blue-500" : ""}>Dashboard</NavLink>
+            <NavLink to="/settings" className={({isActive}) => isActive ? "text-blue-500" : ""}>Paramètres</NavLink>
         </nav>
     )
 }
